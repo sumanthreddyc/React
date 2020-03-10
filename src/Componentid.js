7cbdea5372411b5347c2a9b61e15b0fa81daf0c9
sumanthreddyc <sumanth.challaram@gmail.com>
2020-03-10 20:33:54 +0530

    new commit

--- src/Componentid.js
+++ src/Componentid.js
@@ -6,25 +6,21 @@ class Componentid extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			item: []
+			item: {}
 		}
 	}
 
 	componentWillReceiveProps(nextProps) {
         fetch("https://hacker-news.firebaseio.com/v0/item/{this.nextProps.id}.json?print=pretty")
             .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    item: data
-                })
-            })
+            .then(data => console.log(data))
     }
 
 
 	render() {
 		return(
 			<div>
-				<p>{this.state.item.by}</p>
+				<p>{this.state.item.title}</p>
 			</div>
 		)
 	}
